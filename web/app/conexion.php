7415f289df1cf6e83c2cbae18aacab4eb20739d4
Rafael <r.livise@hotmail.com>
2018-06-28 00:12:02 -0500

    base de datos clearDB

--- web/app/conexion.php
+++ web/app/conexion.php
@@ -1,6 +1,13 @@
 <!--conexion.php-->
 <?php
-$conexion = new mysqli("localhost","root","","ColegioSB");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conexion = new mysqli($server, $username, $password, $db);
 if($conexion)
 {
 	//echo "Conexion exitosa </br>";
